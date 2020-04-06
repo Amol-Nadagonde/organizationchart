397c3479b8ce7a4ac9b000c147af22e0ab2a4271
Amol Nadagonde <amol.nadagonde@borngroup.com>
2020-04-06 17:00:05 +0530

    Updates

---
+++ precache-manifest.49ec1f09269f835752232c90341e1e89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe793a71c6280551a5c0717593adec31",
+    "url": "/organizationchart/index.html"
+  },
+  {
+    "revision": "30d35b13ac075134d05e",
+    "url": "/organizationchart/static/css/main.9fcb8df9.chunk.css"
+  },
+  {
+    "revision": "4fe6b49a56e87922640d",
+    "url": "/organizationchart/static/js/2.ff5b7083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/organizationchart/static/js/2.ff5b7083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30d35b13ac075134d05e",
+    "url": "/organizationchart/static/js/main.d18858ee.chunk.js"
+  },
+  {
+    "revision": "cdf93e2b846439df7352",
+    "url": "/organizationchart/static/js/runtime-main.36c316f1.js"
+  },
+  {
+    "revision": "681f5f07f14c0075ab26c19e011cd0f0",
+    "url": "/organizationchart/static/media/icomoon.681f5f07.ttf"
+  }
+]);
